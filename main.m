255b17f4ea17d662437cbbe4260463edf85a1be5
Felipe <felipe.villalobos.p@usach.cl>
2021-06-13 04:07:46 -0400

    Enviado

--- main.m
+++ main.m
@@ -1,57 +1,71 @@
+%Autores:
+%   -Gary Simken       19.986.690-7
+%   -Felipe Villalobos 20.139.310-8
 clc
 clear all
 close all
-
-
 s = tf('s');
 
+
+%------------ Pregunta 1 ---------------------
 %Funcion 1
 
 H1 = 8/(6*s+2);
 H1_Lazo=feedback(H1,1);
-[y1,t1]=step(H1);
-[y1_Lazo,t1_Lazo]=step(H1_Lazo);
-
-%subplot(2,1,1),plot(t1,y1,'b',t1_Lazo,y1_Lazo,'r')
-%subplot(2,1,2), pzmap(H1,'b',H1_Lazo,'r')
+[y1,t1]=step(H1,20);
+[y1_Lazo,t1_Lazo]=step(H1_Lazo, 20);
 
 
 
+createfigure1(t1,y1,t1_Lazo,y1_Lazo)
 
-%Calculo de la ganancia estatica, polos y ceros
+
+
+
+%Calculo de la ganancia estatica, polos y ceros de lazo abierto
 N1= [8];
 D1= [6 2];
 [ceros1,polos1,gan1] = tf2zp (N1,D1);
+%calculada manualmente
+gan1=4;
+tiempo1=12;
 
+%Calculo de la ganancia estatica, polos y ceros de lazo cerrado
 N1_Lazo= [8];
-D1_Lazo= [6 19];
+D1_Lazo= [6 10];
 [ceros1_Lazo,polos1_Lazo,gan1_Lazo] = tf2zp (N1_Lazo,D1_Lazo);
+%calculada manualmente
+gan1_Lazo=0.8;
+tiempo1_Lazo=2.4;
 
-%pzmap(H1)
 
 %Funcion 2
 H2 = (5*s^2 + 7*s + 1) / (s^2 + 6*s + 3);
 H2_Lazo=feedback(H2,1);
-%step(H2)
-%step(H2_Lazo)
+[y2,t2]=step(H2,18);
+[y2_Lazo,t2_Lazo]=step(H2_Lazo,18);
 
-
-%Calculo de la ganancia estatica, polos y ceros
+createfigure2(t2,y2,t2_Lazo,y2_Lazo)
+%Calculo de la ganancia estatica, polos y ceros de lazo abierto
 N2= [5 7 1];
 D2= [1 6 3];
 [ceros2,polos2,gan2] = tf2zp (N2,D2);
+[frecN2,zeta2,p2]= damp(H2);
 
+tiempo2 = 4/(frecN2(2)*cos(frecN2(2)))
+
+%Calculo de la ganancia estatica, polos y ceros de lazo cerrado
 N2_Lazo= [5 7 1];
 D2_Lazo= [1 13 4];
 [ceros2_Lazo,polos2_Lazo,gan2_Lazo] = tf2zp (N2_Lazo,D2_Lazo);
 
-%pzmap(H2)
+[frecN2_Lazo,zeta2_Lazo,p2_lazo]= damp(H2_Lazo);
+tiempo2_Lazo = 2/(-frecN2_Lazo(2)*cos(frecN2_Lazo(2)))
+%Tiempo2_Lazo=4/(zeta*frecN)
 
+%------------ Pregunta 2 ---------------------
 
-
-%Pregunta 2
-
-
+%se ingresan las funciones de transferencias presentes en el diagrama
 Fcn1 = (4*s)/(10*s^2 + 4);
 Fcn2 = 3/(6* s + 16);
 Fcn3 = (4*s + 10)/( 4* s^3 + 9* s^2 + 5*s);
@@ -80,7 +94,9 @@ Fcn6_ser = sum2 * Fcn6;
 H_final= Fcn1 + Fcn2 + Fcn6_ser;
 
 %Se le aplicara un escalon como entrada
-step(H_final, 10);
+[yH,tH] = step(H_final, 10);
+
+createfigure3(tH,yH)
 
 H_final
 
